5b9b9d0be4a1f766f1767f3a499a07434d18f0cf
Marco <msantiago@linko.mx>
2020-07-26 14:39:09 -0500

    refactor(app): remove components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,22 +5,8 @@ import { HomeUIComponent } from './home-ui/home-ui.component';
 import { ViewBooksComponent } from './view-books/view-books.component';
 import { RegisterBookComponent } from './register-book/register-book.component';
 import { LoanRegistrationComponent } from './loan-registration/loan-registration.component';
-import { registerLocaleData } from '@angular/common';
-import { Vinculo1Component } from './vinculo1/vinculo1.component';
-import { Vinculo2Component } from './vinculo2/vinculo2.component';
-import { Vinculo3Component } from './vinculo3/vinculo3.component';
-import { PageGridComponent } from './page-grid/page-grid.component';
-import { MygridComponent } from './mygrid/mygrid.component';
 
 const routes: Routes = [
-
-  {
-    path: 'grid', component: PageGridComponent
-  },
-  {
-    path: 'mygrid', component: MygridComponent
-  },
-
 
   {
     path: 'login', component: AuthenticationComponent
@@ -30,16 +16,12 @@ const routes: Routes = [
     children: [
       { path: 'registerBook', component: RegisterBookComponent},
       { path: 'viewBooks', component: ViewBooksComponent},
-      {path: 'load-regidtration' , component: LoanRegistrationComponent},
-      {path: 'vincule1', component: Vinculo1Component},
-      {path: 'vincule2', component: Vinculo2Component},
-      {path: 'vincule3', component: Vinculo3Component}
-
+      {path: 'load-regidtration' , component: LoanRegistrationComponent}
     ]
   },
   {
     path: '',
-    redirectTo: 'mygrid',
+    redirectTo: 'login',
     pathMatch: 'full'
   }
 ];
