ef8c9f7ab1288bfe8fce1354d2a3a308452d17b7
Marco <msantiago@linko.mx>
2020-07-31 14:47:00 -0500

    refactor(project): implements angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthenticationComponent } from './authentication/authentication.component';
-import { HomeUIComponent } from './home-ui/home-ui.component';
-import { ViewBooksComponent } from './view-books/view-books.component';
-import { RegisterBookComponent } from './register-book/register-book.component';
-import { LoanRegistrationComponent } from './loan-registration/loan-registration.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './shared/home/home.component';
+
 
 const routes: Routes = [
-
-  {
-    path: 'login', component: AuthenticationComponent
-  },
-  {
-    path: 'home', component: HomeUIComponent,
-    children: [
-      { path: 'registerBook', component: RegisterBookComponent},
-      { path: 'viewBooks', component: ViewBooksComponent},
-      {path: 'load-regidtration' , component: LoanRegistrationComponent}
-    ]
-  },
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  }
+  	
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent,  pathMatch: 'full'},
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
