16c000be20619a4f95b044154b80afb39a7393b6
Ruben <rsantiagoferia@gmail.com>
2020-09-14 16:00:14 -0500

    refactor: students-consult (validations)

--- src/app/pages/students-consult/students-consult.component.ts
+++ src/app/pages/students-consult/students-consult.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {  StudentsServiceService } from 'src/app/shared/services/students-service.service';
 import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { Alert } from 'src/app/utils/alerts';
 
 @Component({
   selector: 'app-students-consult',
@@ -10,21 +11,43 @@ import { MatPaginator, MatTableDataSource } from '@angular/material';
 export class StudentsConsultComponent implements OnInit {
 
   displayedColumns: string[] = ['clave1', 'name', 'sex1', 'cel1', 'edit', 'delete'];
-   dataSource = new MatTableDataSource();
-
+  dataSource = new MatTableDataSource();
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  private alert: Alert = new Alert();
 
   constructor( private _service: StudentsServiceService) { }
 
   ngOnInit() {
     
-    this._service.consultStudents().subscribe(item => {
-      console.log(item);
-      this.dataSource.data = item
+    this.allStudents();
+    this.dataSource.paginator = this.paginator;
+  }
 
-    });
+  allStudents() {
+    this._service.consultStudents()
+                 .subscribe(
+                    (item) => { this.dataSource.data = item }
+                 );
+  }
 
-   // this.dataSource.paginator = this.paginator;
+  questionDelete(student:any) { 
+    
+    this.alert.questions("¿Está seguro de eliminar?")
+              .then( option => {
+                /* condicion ternario */
+                (option.value) ? this.delete(student.id) : null;
+                  /* if (option.value) {
+                    this.delete(student.id);
+                  } */
+              });
+  }
+  delete(id:any) {
+    
+    this._service.delete(id)
+                 .subscribe(
+                    (response) => { this.allStudents(); }
+                 );
   }
 
 }
