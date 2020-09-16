2cb37a642f18a3c55a21c041c9491bd79ae4b0b6
Ruben <rsantiagoferia@gmail.com>
2020-09-16 14:22:10 -0500

    refactor: (educational-staff = forms

--- src/app/pages/educational-staff-consult/educational-staff-consult.component.ts
+++ src/app/pages/educational-staff-consult/educational-staff-consult.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource,MatPaginator } from '@angular/material';
+import { Alert } from 'src/app/utils/alerts';
+import { EducationalStaffServiceService } from 'src/app/shared/services/educational-staff-service.service';
 
 @Component({
   selector: 'app-educational-staff-consult',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./educational-staff-consult.component.sass']
 })
 export class EducationalStaffConsultComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'clave', 'tipePerson', 'telefono', 'edit', 'delete'];
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  private alert: Alert = new Alert();
 
-  constructor() { }
+  constructor(
+    private _service: EducationalStaffServiceService
+
+    
+    ) {
+
+     }
 
   ngOnInit() {
+     
+    this.allEducational();
+    this.dataSource.paginator = this.paginator;
+  }
+  allEducational() {
+    this._service.consult()
+                 .subscribe(
+                    (item) => { this.dataSource.data = item }
+                 );
   }
 
 }
--- src/app/pages/educational-staff/educational-staff.component.ts
+++ src/app/pages/educational-staff/educational-staff.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { EducationalStaffServiceService } from 'src/app/shared/services/educational-staff-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-educational-staff',
@@ -12,19 +14,51 @@ export class EducationalStaffComponent implements OnInit {
   
   constructor(
     private educational: FormBuilder,
+    private service: EducationalStaffServiceService,
+    private router: Router,
   ) {}
 
   ngOnInit() {
-    this.educationalForm = this.educational.group({
-      name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(40),Validators.pattern(/^[A-Za-zñÑáÁéÉíÍóÓúÚüÜ ]*$/)]],
-      surname: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(40),Validators.pattern(/^[A-Za-zñÑáÁéÉíÍóÓúÚüÜ ]*$/)]],
-      mothersuname: ['', [Validators.required, Validators.minLength(3),Validators.maxLength(40), Validators.pattern(/^[A-Za-zñÑáÁéÉíÍóÓúÚüÜ ]*$/) ]],
-      cel:[null, [Validators.required, Validators.minLength(10), Validators.maxLength(10),Validators.pattern(/^[0-9]*$/) ]],
-    });
+    this.validateForm();
+   
   }
+  
+validateForm(){
+  this.educationalForm = this.educational.group({
+    name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(40),Validators.pattern(/^[A-Za-zñÑáÁéÉíÍóÓúÚüÜ ]*$/)]],
+    surname: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(40),Validators.pattern(/^[A-Za-zñÑáÁéÉíÍóÓúÚüÜ ]*$/)]],
+    mothersuname: ['', [Validators.required, Validators.minLength(3),Validators.maxLength(40), Validators.pattern(/^[A-Za-zñÑáÁéÉíÍóÓúÚüÜ ]*$/) ]],
+    cel:['', [Validators.required, Validators.minLength(10), Validators.maxLength(10),Validators.pattern(/^[0-9]*$/) ]],
+    email:[null,[Validators.required, Validators.pattern(/(^[\w\d-!#$%&'*+/=?^`{|}]+(\.[\w\d-!#$%&'*+/=?^`{|}]+)*@[\w\d {\/\-\}]+(\.[\w\d-]+)*(\.[a-z]{2,3})$)/)]],
+    sex:[null, [Validators.required, Validators.pattern(/^[A-Za-zñÑáÁéÉíÍóÓúÚüÜ ]*$/)]],
+    postalCode:['', [Validators.required, Validators.minLength(5), Validators.maxLength(5), Validators.pattern(/^[0-9]*$/) ]],
+    delegation:['', [Validators.required, Validators.minLength(5), Validators.maxLength(40), Validators.pattern(/^[A-Za-zñÑáÁéÉíÍóÓúÚüÜ ]*$/)]],
+    colony:['', [Validators.required, Validators.minLength(5), Validators.maxLength(40), Validators.pattern(/^[A-Za-zñÑáÁéÉíÍóÓúÚüÜ ]*$/) ]],
+    street:['', [Validators.required, Validators.minLength(5), Validators.maxLength(40), Validators.pattern(/^[A-Za-zñÑáÁéÉíÍóÓúÚüÜ ]*$/)]],
+    typePerson:['', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern(/^[A-Za-zñÑáÁéÉíÍóÓúÚüÜ ]*$/)]],
+    degree:['', [Validators.required, Validators.pattern(/^[A-Za-zñÑáÁéÉíÍóÓúÚüÜ ]*$/)]],
+    clave:['', [Validators.required, Validators.minLength(5), Validators.maxLength(20), Validators.pattern(/^[0-9]*$/)]],
 
-  save() {
-    
-  }
+  });
+ }
+save() {
+
+  //if (0) {
+  //  return ;
+  //}
+
+  
+  this.service.create(this.educationalForm.value)
+              .subscribe(
+                (Response)=> {
+              console.log('El elemento fue creado correctamente');
+                }
+                );
+};
+
+testRouter() {
+  this.router.navigate(['/educational-staff/educational-staff-consult']);
+}
+
 
 }
