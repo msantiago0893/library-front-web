a7f30936763289fdf732de970df5e3e2ae3d7759
Marco <msantiago@linko.mx>
2021-05-16 16:15:09 -0500

    feat(app): create component forgot password

---
+++ src/app/auth/forgout-password/forgout-password.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ForgoutPasswordComponent } from './forgout-password.component';
+
+describe('ForgoutPasswordComponent', () => {
+  let component: ForgoutPasswordComponent;
+  let fixture: ComponentFixture<ForgoutPasswordComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ForgoutPasswordComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ForgoutPasswordComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/auth/forgout-password/forgout-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgout-password',
+  templateUrl: './forgout-password.component.html',
+  styleUrls: ['./forgout-password.component.sass']
+})
+export class ForgoutPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
