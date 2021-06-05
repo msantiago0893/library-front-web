dc62135f9ed783570afd184c33d0a5f40951f1af
Marco <msantiago@linko.mx>
2021-06-05 18:59:23 -0500

    feat(components): add component prv-section-loader

---
+++ src/app/shared/components/prv-section-loader/prv-section-loader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prv-section-loader',
+  templateUrl: './prv-section-loader.component.html',
+  styleUrls: ['./prv-section-loader.component.sass']
+})
+export class PrvSectionLoaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
