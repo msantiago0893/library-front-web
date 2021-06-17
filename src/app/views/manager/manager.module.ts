f4f33387110ef0f60f01becf245ac3b0e23936d5
Marco <msantiago@linko.mx>
2021-06-17 00:10:19 -0500

    feat(manager): add component detail-book

--- src/app/views/manager/manager.module.ts
+++ src/app/views/manager/manager.module.ts
@@ -3,25 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { ManagerRoutingModule } from './manager-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpErrorInterceptor } from 'src/app/core/interceptors/error.service';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ManagerComponent } from './manager.component';
 import { BooksAllComponent } from './books-all/books-all.component';
-
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HttpErrorInterceptor } from 'src/app/core/interceptors/error.service';
 import { UserComponent } from './user/user.component';
+import { DetailBookComponent } from './detail-book/detail-book.component';
 @NgModule({
   declarations: [
     DashboardComponent,
     ManagerComponent,
     BooksAllComponent,
-    UserComponent
+    UserComponent,
+    DetailBookComponent
   ],
   imports: [
     CommonModule,
     ManagerRoutingModule,
     SharedModule,
+  ],
+  entryComponents: [
+    DetailBookComponent
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
