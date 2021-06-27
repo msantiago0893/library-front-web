4700a16b1b4c0e208e7835b6b7908fc40ab1dc9b
Gabo <ruben santiago feria>
2021-06-27 18:30:44 -0500

    fix(app.module): resolved conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MAT_DATE_LOCALE} from '@angular/material';
 
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
