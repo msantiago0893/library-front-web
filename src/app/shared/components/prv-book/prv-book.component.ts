07e9fd9bb3e3362548d79ee56e708a7412744bb3
Marco <msantiago@linko.mx>
2021-06-27 19:43:52 -0500

    refactor(prv-book): resolve conflict

--- src/app/shared/components/prv-book/prv-book.component.ts
+++ src/app/shared/components/prv-book/prv-book.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { BookService } from '@services/book.service';
 
 import * as Regex from '@constant/regex';
+import { Bookadapter } from './domin/bookadapter';
 import * as Date from '@constant/date';
 import { Alert } from '@utils/alerts';
 import { MESSAGE, TYPE_ALERT } from '@constant/catalog-alert';
@@ -17,6 +18,7 @@ import { MESSAGE, TYPE_ALERT } from '@constant/catalog-alert';
 export class PrvBookComponent implements OnInit {
 
   bookForm : FormGroup;
+  maxDate = Date.previousDate();
 
   @Input()  book : any;
   @Input() updateTable : Function;
@@ -42,11 +44,24 @@ export class PrvBookComponent implements OnInit {
 
     if (this.book) {
 
-     
+      this._service.update(this.book.id, new Bookadapter(this.bookForm.value))
+        .subscribe(() => {
+          Alert.msgTimer(TYPE_ALERT.SUCCESS,MESSAGE.MODIFY);
+          this.updateTable();
+          this.toBack();
+        },
+      );
 
     } else {
 
-
+      this._service.add(new Bookadapter(this.bookForm.value))
+        .subscribe(
+          () => {
+          Alert.msgTimer(TYPE_ALERT.SUCCESS, MESSAGE.ADD);
+          this.updateTable();
+          this.toBack();
+          }
+        );
     }
   }
 
@@ -98,7 +113,10 @@ export class PrvBookComponent implements OnInit {
         Validators.maxLength(10),
         Validators.pattern(Regex.numeric)
       ]],
-
+      yearEdicion:[{ value:'', disabled:true }, [
+        Validators.required,
+        Validators.pattern(Regex.date)
+      ]],
       photo: ['']
     });
   }
